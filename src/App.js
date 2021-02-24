376c305dc3d921376ca8aaefb3dc0db2ba3832ab
avinash-vellineni-hertz-com <avinash.vellineni@hertz.com>
2021-02-24 10:48:22 -0600

    Router additions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          <Route path="/" component={BurgerBuilder}></Route>
+          <Route path="/checkout" component={}></Route>
         </Layout>
       </div>
     );
